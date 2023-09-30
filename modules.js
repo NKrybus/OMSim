1ca4388007f4fb222f44702ddd7b8c7a2e074bef
crilozmar <crilozmar@users.noreply.github.com>
2023-09-30 18:08:44 +0000

    deploy: c366b020aa38d6380285cd16e0d8bb25a044b110

--- modules.js
+++ modules.js
@@ -3,6 +3,6 @@ var modules =
     [ "OMSim framework and geometry", "group__common.html", "group__common" ],
     [ "Effective area", "group___effective_area.html", "group___effective_area" ],
     [ "Radioactive decays", "group__radioactive.html", "group__radioactive" ],
-    [ "Supernova Studies", "group__sngroup.html", "group__sngroup" ],
+    [ "Supernova Studies", "group__sngroup.html", null ],
     [ "Bubble column", "group__bubble.html", null ]
 ];
